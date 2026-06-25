bdd90de2840aa944342fd4d65a53d0bc8d4bd398
Wandreson Silva <wandresonpereiradasilva792@gmail.com>
2026-06-25 20:32:30 -0300

    Create teste-logico-python.py

---
+++ teste-logico-python.py
@@ -0,0 +1,39 @@
+numero1 = int(input("Digite um número entre 1 e 100: "))
+numero2 = int(input("Agora digite o número 2:" ))
+
+resultado1 = numero1*numero2
+
+# print(resultado1)
+
+numero3 = int(input("Agora digite o número 5: "))
+
+resultado2 = resultado1+numero3
+
+# print(resultado2)
+
+numero4 = int(input("Agora digite o número 50: "))
+
+resultado3 = resultado2*numero4
+
+# print(resultado3)
+
+numero5 = int(input("Se você já fez aniversário este ano, digite 1776. Agora se você ainda não fez aniversário, digite 1775: "))
+
+resultado4 = resultado3+numero5
+
+# print(resultado4)
+
+numero6 = int(input("Agora digite o ano que você nasceu: "))
+
+resultado5 = resultado4-numero6
+
+print(resultado5)
+
+print("O Resultado é igual {}". format(resultado5))
+
+print("PARA O RESULTADO FINAL {} - O PRIMEIRO DÍGITO, É O NÚMERO QUE VOCÊ PENSOU, E OS DOIS ÚLTIMOS DÍGITOS É A SUA IDADE. E AI, ACERTEI?". format(resultado5))
+
+
+
+
+        
